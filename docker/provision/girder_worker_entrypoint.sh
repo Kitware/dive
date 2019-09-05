9f26b6d979f23946012bae865d330315075f45a3
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-09-04 22:00:38 -0400

    Implement girder_worker task to run viame pipeline

--- docker/provision/girder_worker_entrypoint.sh
+++ docker/provision/girder_worker_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sleep 5
-cd /home/viame_girder && pip install .
+cd /home/viame_girder && pip install . -U
 python3 /home/provision/girder.py
 girder-worker-config set celery broker amqp://guest:guest@rabbit/
 girder-worker-config set girder_worker plugins_enabled girder_io
