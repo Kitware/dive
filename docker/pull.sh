148498ddf7f7cf07634e3634972cbd41059c6191
Matthew Ma <matthew.ma@kitware.com>
2019-12-13 15:05:31 -0500

    demo docker scripts

---
+++ docker/pull.sh
@@ -0,0 +1,3 @@
+docker login gitlab.kitware.com:4567
+docker pull gitlab.kitware.com:4567/opengeoscience/viameweb/app/girder_worker
+docker pull gitlab.kitware.com:4567/opengeoscience/viameweb/base/girder_worker
