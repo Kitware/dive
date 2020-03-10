d883a85b92eba37ad8de11ef6314d45aee983878
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-03-10 19:22:46 -0400

    Move girder init file, use in main girder container

--- docker/provision/girder_entrypoint.sh
+++ docker/provision/girder_entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Install viame plugin
-cd /home/viame_girder && pip install -e . -U
-girder serve --database mongodb://mongo:27017/girder --host 0.0.0.0
+cd /home/viame_girder && pip3 install -e . -U
+python3 /home/provision/init_girder.py
+girder serve --database $MONGO_URI --host 0.0.0.0
--- docker/provision/girder_worker_entrypoint.sh
+++ docker/provision/girder_worker_entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sleep 25
 cd /home/viame_girder && pip install -e .
-python3 /home/provision/girder.py
 girder-worker-config set celery broker amqp://guest:guest@rabbit/
 girder-worker-config set girder_worker plugins_enabled girder_io
 girder-worker -l info --broker amqp://guest:guest@rabbit/
