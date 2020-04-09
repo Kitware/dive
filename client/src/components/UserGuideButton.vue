6ec6594a2360d57f3fbcf5b1515d10cbe5ea0c13
Brandon Davis <brandon.davis@kitware.com>
2020-04-09 10:46:41 -0400

    Refactor to individual component

---
+++ client/src/components/UserGuideButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn
+    dense
+    depressed
+    href="https://github.com/VIAME/VIAME-Web/wiki/User-Documentation"
+    target="_blank"
+    color="secondary darken-2"
+    class="mx-3"
+  >
+    User Guide
+    <v-icon class="pl-2">mdi-help-circle</v-icon>
+  </v-btn>
+</template>
