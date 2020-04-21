adac5127bdd3dc90ab48f55e578b007c47aa16ab
Bryon Lews <Bryon.Lewis@kitware.com>
2020-04-21 12:33:03 -0400

    Updating styling and sizing

--- client/src/components/ContextualHelp.vue
+++ client/src/components/ContextualHelp.vue
@@ -10,47 +10,87 @@ export default {
   },
   data() {
     return {
-      states: {
-        default: {
-          description: 'Select a track to view more information or advance through the frames',
-          shortcuts: [
+      categories: [
+        {
+          name: 'Interactions',
+          data: [
             {
-              name: 'Select Track', icon: 'mouse', action: 'Left Click Mouse', description: 'Left click a rectangle to select a track',
+              name: 'Select Track', icon: ['mouse'], actions: ['Left Click Mouse'], description: 'Left click a rectangle to select a track',
             },
             {
-              name: 'Edit Track', icon: 'mouse', action: 'Right Click Mouse', description: 'Right click a track to enter Edit Mode',
+              name: 'Zoom In/Out', icon: ['mouse'], actions: ['Scrollwheel Up/Down'], description: 'use scrollwheel to zoom in and out',
             },
             {
-              name: 'Zoom In/Out', icon: 'mouse', action: 'Scrollwheel Up/Down', description: 'use scrollwheel to zoom in and out',
+              name: 'Zoom Area', icon: ['mouse'], actions: ['Shift + Mouse Movement'], description: 'Zoom into a specific area',
             },
-            {
-              name: 'Zoom Area', icon: 'mouse', action: 'Shft + Mouse Movement', description: 'Zoom into a specific area',
-            },
-            {
-              name: 'Fish Head/Tail', icon: 'keyboard', action: 'H key - heads  T key - tails', description: 'While a track is selected add head/tail annotations',
-            },
-            {
-              name: 'Play', icon: 'keyboard', action: 'Spacebar', description: 'Spacebar will pause and start tracks',
-            },
-            {
-              name: 'Frame Forward/Back', icon: 'keyboard', action: 'F key - forward one frame, D key - back one frame', description: 'use scrollwheel to zoom in and out',
-            },
+
 
           ],
         },
-        edit: {
-          description: 'Resize a track for the current frame or move it around.  Be sure to click the save icon after editing to save multiple edits.',
-          shortcuts: [
-
+        {
+          name: 'Editing',
+          data: [
+            {
+              name: 'Edit Track', icon: ['mouse'], actions: ['Right Click Mouse'], description: 'Right click a track to enter Edit Mode',
+            },
+            {
+              name: 'Add Head/Tail', icon: ['keyboard'], actions: ['H or G Key - Head', 'T or Y Key - Tail'], description: 'While a track is selected add head/tail annotations',
+            },
+            {
+              name: 'Delete Head/Tail', icon: ['keyboard'], actions: ['Q Key'], description: 'While a track is selected add head/tail annotations',
+            },
           ],
         },
-      },
+        {
+          name: 'Playback',
+          data: [
+            {
+              name: 'Play', icon: ['keyboard'], actions: ['Spacebar'], description: 'Spacebar will pause and start tracks',
+            },
+            {
+              name: 'Frame Forward / Back', icon: ['keyboard'], actions: ['F Key - frame forward', 'D Key - frame back'], description: 'use scrollwheel to zoom in and out',
+            },
+          ],
+        },
+      ],
     };
   },
 };
 </script>
-<template />
-
-<style>
-
+<template>
+  <div>
+    <v-card
+      v-for="(category, index) in categories"
+      id="helpdialog"
+      :key="index"
+      elevation="12"
+      class="mb-5 pa-2"
+    >
+      <v-row
+        v-for="(item, i) in category.data"
+        :key="i"
+      >
+        <v-col cols="3">
+          {{ item.name }}
+        </v-col>
+        <v-col cols="1">
+          <v-icon>mdi-{{ item.icon[0] }}</v-icon>
+        </v-col>
+        <v-col col="8">
+          <div
+            v-for="(action, actionIndex) in item.actions"
+            :key="actionIndex"
+          >
+            {{ action }}
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+<style lang="scss" scoped>
+#helpdialog{
+  font-family: monospace;
+  font-size: 12px;
+}
 </style>
--- client/src/components/UserGuideButton.vue
+++ client/src/components/UserGuideButton.vue
@@ -1,15 +1,64 @@
+<script>
+import ContextualHelp from '@/components/ContextualHelp.vue';
+
+export default {
+  components: {
+    ContextualHelp,
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
 <template>
-  <v-btn
-    dense
-    depressed
-    href="https://github.com/VIAME/VIAME-Web/wiki/User-Documentation"
-    target="_blank"
-    color="secondary darken-2"
-    class="mx-3"
+  <v-dialog
+    v-model="dialog"
+    width="600"
   >
-    User Guide
-    <v-icon class="pl-2">
-      mdi-help-circle
-    </v-icon>
-  </v-btn>
+    <template #activator="{on}">
+      <v-btn
+        dense
+        depressed
+        target="_blank"
+        color="secondary darken-2"
+        class="mx-3"
+        v-on="on"
+      >
+        User Guide
+        <v-icon class="pl-2">
+          mdi-help-circle
+        </v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        Help
+      </v-card-title>
+      <v-card-text>
+        <contextual-help />
+        <v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              dense
+              depressed
+              href="https://github.com/VIAME/VIAME-Web/wiki/User-Documentation"
+              target="_blank"
+              color="secondary darken-2"
+              class="mx-3"
+            >
+              User Guide
+              <v-icon class="pl-2">
+                mdi-help-circle
+              </v-icon>
+            </v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-card-text>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
