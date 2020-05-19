1f8ac616071f2ef029689a32a8e0c67885784c01
Bryon Lews <Bryon.Lewis@kitware.com>
2020-05-19 15:00:29 -0400

    Changed to be more clear that the pipelines are objects

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     location: null,
-    pipelines: [],
+    pipelines: null,
   },
   mutations: {
     setLocation(state, location) {
@@ -20,8 +20,26 @@ export default new Vuex.Store({
   },
   actions: {
     async fetchPipelines({ commit, state }) {
-      if (state.pipelines.length === 0) {
+      if (state.pipelines === null) {
         const { data } = await getPipelineList();
+        Object.keys(data).forEach((key) => {
+          const category = data[key];
+          if (category.pipes.length > 0) {
+            category.pipes.sort((a, b) => {
+              if (a.name && b.name) {
+                const aName = a.name.toUpperCase();
+                const bName = b.name.toUpperCase();
+                if (aName > bName) {
+                  return 1;
+                }
+                if (aName < bName) {
+                  return -1;
+                }
+              }
+              return 0;
+            });
+          }
+        });
         commit('setPipelines', data);
         return data;
       }
