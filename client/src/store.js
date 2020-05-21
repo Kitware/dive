dc43a6c48f6c83f9b8ba4cdd4691808f75184192
BryonLewis <61746913+BryonLewis@users.noreply.github.com>
2020-05-21 12:33:25 -0400

    122 sort pipelines (#132)
    * Changed to be more clear that the pipelines are objects
    
    * Updated the pipelines data to always be an object
    
    * requested changes

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     location: null,
-    pipelines: [],
+    pipelines: null,
   },
   mutations: {
     setLocation(state, location) {
@@ -20,8 +20,22 @@ export default new Vuex.Store({
   },
   actions: {
     async fetchPipelines({ commit, state }) {
-      if (state.pipelines.length === 0) {
+      if (state.pipelines === null) {
         const { data } = await getPipelineList();
+        // Sort list of pipelines in each category by name
+        Object.values(data).forEach((category) => {
+          category.pipes.sort((a, b) => {
+            const aName = a.name.toLowerCase();
+            const bName = b.name.toLowerCase();
+            if (aName > bName) {
+              return 1;
+            }
+            if (aName < bName) {
+              return -1;
+            }
+            return 0;
+          });
+        });
         commit('setPipelines', data);
         return data;
       }
