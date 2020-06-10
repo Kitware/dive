b9a0e0a99f793d098ef2e28a1dad8bce35eec007
Brandon Davis <brandon.davis@kitware.com>
2020-06-10 10:51:39 -0400

    Update add extra tests

--- client/src/lib/utils.spec.ts
+++ client/src/lib/utils.spec.ts
@@ -15,4 +15,18 @@ describe('updateSubset', () => {
     const sub = [1, 2, 3];
     expect(updateSubset(oldsuper, newsuper, sub)).toEqual([1, 2, 4]);
   });
+
+  it('should ignore duplicate values', () => {
+    const oldsuper = [0, 1, 2, 1, 2, 3, 10, 20];
+    const newsuper = [0, 1, 1, 2, 4, 3, 3, 4];
+    const sub = [1];
+    expect(updateSubset(oldsuper, newsuper, sub)).toEqual([1, 4]);
+  });
+
+  it('should not care if the sub-array has values outside the superset', () => {
+    const oldsuper = [1, 3, 4];
+    const newsuper = [2, 3, 4];
+    const sub = [100, 10000, 1];
+    expect(updateSubset(oldsuper, newsuper, sub)).toEqual([100, 10000, 2]);
+  });
 });
