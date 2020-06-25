5d20ef04040dbc83eb47bc9dd45e354c61a87a6e
Brandon Davis <brandon.davis@kitware.com>
2020-06-25 10:04:27 -0400

    Didn't need to depend here

--- client/src/views/TrackViewer/Layers.vue
+++ client/src/views/TrackViewer/Layers.vue
@@ -186,7 +186,6 @@ export default defineComponent({
       props.editingTrack,
       props.selectedTrackId,
       props.featurePointing,
-      props.typeStyling,
     ], () => {
       updateLayers();
     });
