f1d479d245466f715e43940fbaa9bfa60c53be23
mattrix27 <mtung@mit.edu>
2020-06-30 17:14:56 -0400

    clean up code and implement on Home

--- client/src/constants.js
+++ client/src/constants.js
@@ -7,27 +7,8 @@ const MediaTypes = {
   [VideoType]: 'video',
 };
 
-// Supported File Types
-const videoFileTypes = ['.mp4', '.avi', '.mov', '.mpg'];
-const webFriendlyImageFileTypes = ['.jpg', '.jpeg', '.png'];
-const imageFileTypes = [...webFriendlyImageFileTypes, '.bmp', '.sgi', '.tif', '.tiff', '.pgm'];
-
-// Utility for commonly used regular expressions
-const videoFilesRegEx = new RegExp(`${videoFileTypes.join('$|')}$`, 'i');
-const imageFilesRegEx = new RegExp(`${imageFileTypes.join('$|')}$`, 'i');
-const webFriendlyImageRegEx = new RegExp(`${webFriendlyImageFileTypes.join('$|')}$`, 'i');
-
-
 export {
   ImageSequenceType,
   VideoType,
   MediaTypes,
-
-  videoFileTypes,
-  webFriendlyImageFileTypes,
-  imageFileTypes,
-
-  videoFilesRegEx,
-  imageFilesRegEx,
-  webFriendlyImageRegEx,
 };
