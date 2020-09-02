dc0c8f7a94081c1cc7cf7a83c840f6c7326f4dc4
Bryon Lews <Bryon.Lewis@kitware.com>
2020-09-02 11:18:24 -0400

    Adding in geoJSON testing and attribute testing

--- server/tests/test_deserialize_viame_csv.py
+++ server/tests/test_deserialize_viame_csv.py
@@ -11,7 +11,17 @@ test_tuple = [
             "0,1.png,0,884,510,1219,737,1,-1,ignored,1",
             "0,2.png,1,111,222,3333,444,1,-1,typestring,1",
             "1,1.png,0,747,457,1039,633,1,-1,type2,1",
+            # Keypoint testing with HeadTails Line
             "2,3.png,2,10,50,20,35,1,-1,type3,0.765,(kp) head 22.4534 45.6564,(kp) tail 55.232 22.3445",
+            # Keypoint without HeadTails Line
+            "2,4.png,3,10,50,20,35,1,-1,type3,0.765,(kp) head 22.4534 45.6564",
+            # Multiple ConfidencePair Test
+            "3,4.png,4,10,10,20,20,1,-1,type1,0.89,type2,0.65",
+            # Polygon Test with floats
+            "4,5.png,5,10,10,20,20,1,-1,type1,0.89,type2,0.65,(poly) 1 2.34 3 4 5 6 7 8.08 9 10",
+            # Track and Frame Attr testing
+            "5,6.png,6,10,10,20,20,1,-1,type1,0.89,(atr) attrNAME spaced attr name,(trk-atr) booleanAttr true",
+
         ],
         {
             "0": {
@@ -92,15 +102,96 @@ test_tuple = [
                                 },
                             ],
                         },
+                    },
+                    {
+                        "frame": 3,
+                        "bounds": [10, 50, 20, 35],
+                        "keyframe": True,
+                        "interpolate": False,
+                        "geometry": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": "head"},
+                                    "geometry": {
+                                        "type": "Point",
+                                        "coordinates": [22.4534, 45.6564],
+                                    },
+                                },
+                            ],
+                        },
                     }
                 ],
                 "begin": 2,
-                "end": 2,
+                "end": 3,
+            },
+            "3": {
+                "trackId": 3,
+                "attributes": {},
+                "confidencePairs": [["type1", 0.89], ["type2", 0.65]],
+                "features": [
+                    {
+                        "frame": 4,
+                        "bounds": [10, 10, 20, 20],
+                        "keyframe": True,
+                        "interpolate": False,
+                    },
+                ],
+                "begin": 4,
+                "end": 4,
+            },
+            "4": {
+                "trackId": 4,
+                "attributes": {},
+                "confidencePairs": [["type1", 0.89], ["type2", 0.65]],
+                "features": [
+                    {
+                        "frame": 5,
+                        "bounds": [10, 10, 20, 20],
+                        "keyframe": True,
+                        "interpolate": False,
+                        "geometry": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": ""},
+                                    "geometry": {
+                                        "type": "Polygon",
+                                        "coordinates": [[[1.0, 2.34], [3.0, 4.0], [5.0, 6.0], [7.0, 8.08], [9.0, 10.0]]],
+                                    },
+                                },
+                            ]
+                        },
+                    },
+                ],
+                "begin": 5,
+                "end": 5,
+            },
+            "5": {
+                "trackId": 5,
+                "attributes": {
+                    "booleanAttr": True
+                },
+                "confidencePairs": [["type1", 0.89]],
+                "features": [
+                    {
+                        "frame": 6,
+                        "bounds": [10, 10, 20, 20],
+                        "keyframe": True,
+                        "interpolate": False,
+                        "attributes": {
+                            "attrNAME": "spaced attr name"
+                        }
+                    },
+                ],
+                "begin": 6,
+                "end": 6,
             },
         },
     ),
 ]
-
 
 @pytest.mark.parametrize("input,expected", test_tuple)
 def test_read_csv(input: List[str], expected: Dict[str, dict]):
--- server/tests/test_serialize_viame_csv.py
+++ server/tests/test_serialize_viame_csv.py
@@ -84,6 +84,119 @@ test_tuple = [
             "0,4.png,3,4,4,8,8,0.9,-1,baz,0.1,foo,0.2,bar,0.9",
         ],
     ),
+    # Testing geoJSON Features for Keypoints (head/tails) and Polygons
+    (
+        {
+            "1": {
+                "begin": 1,
+                "end": 3,
+                "trackId": 0,
+                "features": [
+                    {
+                        "frame": 1,
+                        "bounds": [2, 2, 4, 4],
+                        "interpolate": True,
+                        "keyframe": True,
+                        "geometry": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": "head"},
+                                    "geometry": {
+                                        "type": "Point",
+                                        "coordinates": [22.4534, 45.6564],
+                                    },
+                                },
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": "tail"},
+                                    "geometry": {
+                                        "type": "Point",
+                                        "coordinates": [55.232, 22.3445],
+                                    },
+                                },
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": "HeadTails"},
+                                    "geometry": {
+                                        "coordinates": [
+                                            [22.4534, 45.6564],
+                                            [55.232, 22.3445],
+                                        ],
+                                        "type": "LineString",
+                                    },
+                                },
+                            ],
+                        },                    
+                    },
+                    {
+                        "frame": 3,
+                        "bounds": [4, 4, 8, 8],
+                        "interpolate": True,
+                        "keyframe": True,
+                        "geometry": {
+                            "type": "FeatureCollection",
+                            "features": [
+                                {
+                                    "type": "Feature",
+                                    "properties": {"key": ""},
+                                    "geometry": {
+                                        "type": "Polygon",
+                                        "coordinates": [[[1.0, 2.34], [3.0, 4.0], [5.0, 6.0], [7.0, 8.08], [9.0, 10.0]]],
+                                    },
+                                },
+                            ]
+                        },
+                    },
+                ],
+                "confidencePairs": [["foo", 0.2], ["bar", 0.9], ["baz", 0.1]],
+            }
+        },
+        [
+            "0,2.png,1,2,2,4,4,0.9,-1,baz,0.1,foo,0.2,bar,0.9,(kp) head 22 45,(kp) tail 55 22",
+            "0,3.png,2,3,3,6,6,0.9,-1,baz,0.1,foo,0.2,bar,0.9",
+            "0,4.png,3,4,4,8,8,0.9,-1,baz,0.1,foo,0.2,bar,0.9,(poly) 1 2 3 4 5 6 7 8 9 10",
+        ],
+    ),
+    (
+        {
+            "0": {
+                "trackId": 0,
+                "attributes": {
+                    "trackATTR": "TestTrack ATTR With Space",
+                },
+                "confidencePairs": [["typestring", 1.0]],
+                "features": [
+                    {
+                        "frame": 0,
+                        "bounds": [884, 510, 1219, 737],
+                        "keyframe": True,
+                        "interpolate": False,
+                        "attributes":{
+                            "detectionAttr": "frame 0 attr"
+                        }
+                    },
+                    {
+                        "frame": 1,
+                        "bounds": [111, 222, 3333, 444],
+                        "keyframe": True,
+                        "interpolate": False,
+                        "attributes":{
+                            "detectionAttr": "frame 1 attr"
+                        }
+
+                    },
+                ],
+                "begin": 0,
+                "end": 1,
+            },
+        },
+        [
+            "0,1.png,0,884,510,1219,737,1.0,-1,typestring,1.0,(atr) detectionAttr frame 0 attr,(trk-atr) trackATTR TestTrack ATTR With Space",
+            "0,2.png,1,111,222,3333,444,1.0,-1,typestring,1.0,(atr) detectionAttr frame 1 attr,(trk-atr) trackATTR TestTrack ATTR With Space",
+        ],
+    ),
 ]
 
 
