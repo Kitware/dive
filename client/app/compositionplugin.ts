a2a98496df6c09924de78a1bf2b4fd269b40ac7b
Brandon Davis <brandon.davis@kitware.com>
2020-10-09 13:13:34 -0400

    Add rollup for module release

--- client/app/compositionplugin.ts
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
-
-Vue.use(VueCompositionApi);
-
-export default VueCompositionApi;
---
+++ client/src/index.ts
@@ -0,0 +1,19 @@
+import * as listUtils from './listUtils';
+import * as providers from './provides';
+import track from './track';
+import * as utils from './utils';
+
+import * as use from './use';
+import * as layers from './layers';
+// import * as components from './components';
+
+export {
+  listUtils,
+  providers,
+  track,
+  utils,
+
+  use,
+  layers,
+  // components,
+};
