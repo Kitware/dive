Kitware/dive

commit 80483a9f7ff0b1f3f5c58bb0b44bbcc9700f85e7

Use provide instead of PropType<Ref<...>> (#361)
* First example

* wip

* Done

* add README update

* Addres comments

* Fix snackbar warning

client/src/components/TrackItem.vue
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
+import {
+  defineComponent, computed, watch, reactive, PropType, toRef, ref,
+} from '@vue/composition-api';
 import TooltipBtn from 'vue-media-annotator/components/TooltipButton.vue';
+import { useFrame } from 'vue-media-annotator/provides';
+import Track from 'vue-media-annotator/track';
 
-export default {
+export default defineComponent({
   name: 'TrackItem',
 
   components: { TooltipBtn },
@@ -11,16 +16,8 @@ export default {
       type: String,
       required: true,
     },
-    types: {
-      type: Array,
-      required: true,
-    },
-    frame: {
-      type: Object,
-      required: true,
-    },
     track: {
-      type: Object,
+      type: Object as PropType<Track>,
       required: true,
     },
     inputValue: {
@@ -41,47 +38,27 @@ export default {
     },
   },
 
-  data() {
-    return {
-      value: this.trackType,
+  setup(props, { root, emit }) {
+    const vuetify = root.$vuetify;
+    const frameRef = useFrame();
+    const trackTypeRef = toRef(props, 'trackType');
+    const typeInputBoxRef = ref(undefined as undefined | HTMLInputElement);
+    const data = reactive({
+      trackTypeValue: props.trackType,
       skipOnFocus: false,
-    };
-  },
-  computed: {
-    isTrack() {
-      return this.track.length > 1 || this.feature.shouldInterpolate;
-    },
-    trackId() {
-      return this.track.trackId;
-    },
-    /**
-     * Sets styling for the selected track
-     * Sets the background accent color to have a slight
-     * opacity so it isn't overwhelming
-     */
-    style() {
-      if (this.selected) {
-        return {
-          'background-color': `${this.$vuetify.theme.themes.dark.accentBackground}`,
-        };
-      }
-      return {};
-    },
-    /**
-     * Use of revision is safe because it will only create
-     * a dependency when selected is true.
-     */
-    feature() {
-      if (this.track.revision.value) {
-        const { features, interpolate } = this.track.canInterpolate(this.frame.value);
+    });
+
+    /* Use of revision is safe because it will only create a dependency when track is selected */
+    const feature = computed(() => {
+      if (props.track.revision.value) {
+        const { features, interpolate } = props.track.canInterpolate(frameRef.value);
         const [real, lower, upper] = features;
         return {
           real,
           lower,
           upper,
-          // Upper an lower are always going to be keyframes
-          targetKeyframe: real?.isKeyframe ? real : (lower || upper),
           shouldInterpolate: interpolate,
+          targetKeyframe: real?.keyframe ? real : (lower || upper),
           isKeyframe: real?.keyframe,
         };
       }
@@ -93,70 +70,108 @@ export default {
         shouldInterpolate: false,
         isKeyframe: false,
       };
-    },
-  },
-  watch: {
-    trackType(val) {
-      this.value = val;
-    },
-  },
-  methods: {
-    focusType() {
-      if (this.selected) {
-        this.skipOnFocus = true;
-        this.$refs.typeInputBox.focus();
-        this.$refs.typeInputBox.select();
+    });
+
+    /* isTrack distinguishes between track and detection */
+    const isTrack = computed(() => props.track.length > 1 || feature.value.shouldInterpolate);
+
+    /* Sets styling for the selected track */
+    const style = computed(() => {
+      if (props.selected) {
+        return {
+          'background-color': `${vuetify.theme.themes.dark.accentBackground}`,
+        };
       }
-    },
-    blurType(e) {
-      e.target.blur();
-    },
-    onBlur() {
-      if (this.value === '') {
-        this.value = this.trackType;
-      } else if (this.value !== this.trackType) {
-        this.$emit('type-change', this.value);
+      return {};
+    });
+
+    /* Update internal model if external prop changes */
+    watch(trackTypeRef, (val) => { data.trackTypeValue = val; });
+
+    function focusType() {
+      if (props.selected && typeInputBoxRef.value !== undefined) {
+        data.skipOnFocus = true;
+        typeInputBoxRef.value.focus();
+        typeInputBoxRef.value.select();
       }
-    },
-    onFocus() {
-      if (!this.skipOnFocus) {
-        this.value = '';
+    }
+
+    function blurType(e: InputEvent) {
+      (e.target as HTMLInputElement).blur();
+    }
+
+    function onBlur() {
+      if (data.trackTypeValue === '') {
+        data.trackTypeValue = props.trackType;
+      } else if (data.trackTypeValue !== props.trackType) {
+        emit('type-change', data.trackTypeValue);
       }
-      this.skipOnFocus = false;
-    },
-    toggleKeyframe() {
-      if (this.feature.real && !this.feature.isKeyframe) {
-        this.track.setFeature({
-          ...this.feature.real,
-          frame: this.frame.value,
+    }
+
+    function onFocus() {
+      if (!data.skipOnFocus) {
+        data.trackTypeValue = '';
+      }
+      data.skipOnFocus = false;
+    }
+
+    function toggleKeyframe() {
+      const f = feature.value;
+      if (f.real && f.isKeyframe) {
+        props.track.setFeature({
+          ...f.real,
+          frame: frameRef.value,
           keyframe: true,
         });
       } else {
-        this.track.deleteFeature(this.frame.value);
+        props.track.deleteFeature(frameRef.value);
       }
-    },
-    toggleInterpolation() {
-      if (this.feature.targetKeyframe) {
-        this.track.setFeature({
-          ...this.feature.targetKeyframe,
-          interpolate: !this.feature.shouldInterpolate,
+    }
+
+    function toggleInterpolation() {
+      const f = feature.value;
+      if (f.targetKeyframe) {
+        props.track.setFeature({
+          ...f.targetKeyframe,
+          interpolate: !f.shouldInterpolate,
         });
       }
-    },
-    gotoNext() {
-      const nextFrame = this.track.getNextKeyframe(this.frame.value + 1);
+    }
+
+    function gotoNext() {
+      const nextFrame = props.track.getNextKeyframe(frameRef.value + 1);
       if (nextFrame !== undefined) {
-        this.$emit('seek', nextFrame);
+        emit('seek', nextFrame);
       }
-    },
-    gotoPrevious() {
-      const previousFrame = this.track.getPreviousKeyframe(this.frame.value - 1);
+    }
+
+    function gotoPrevious() {
+      const previousFrame = props.track.getPreviousKeyframe(frameRef.value - 1);
       if (previousFrame !== undefined) {
-        this.$emit('seek', previousFrame);
+        emit('seek', previousFrame);
       }
-    },
+    }
+
+    return {
+      /* data */
+      data,
+      feature,
+      isTrack,
+      style,
+      typeInputBox: typeInputBoxRef,
+      frame: frameRef,
+      /* methods */
+      blurType,
+      focusType,
+      gotoNext,
+      gotoPrevious,
+      onBlur,
+      onFocus,
+      toggleInterpolation,
+      toggleKeyframe,
+    };
   },
-};
+});
 </script>
 
 <template>
@@ -180,7 +195,7 @@ export default {
         open-delay="200"
         bottom
         max-width="200"
-        :disabled="trackId.toString().length < 8"
+        :disabled="track.trackId.toString().length < 8"
       >
         <template #activator="{ on }">
           <div
@@ -188,15 +203,15 @@ export default {
             v-on="on"
             @click.self="$emit('click')"
           >
-            {{ trackId }}
+            {{ track.trackId }}
           </div>
         </template>
-        <span> {{ trackId }} </span>
+        <span> {{ track.trackId }} </span>
       </v-tooltip>
       <v-spacer />
       <input
-        ref="typeInputBox"
-        v-model="value"
+        ref="typeInputBoxRef"
+        v-model="data.trackTypeValue"
         type="text"
         list="allTypesOptions"
         class="input-box"
@@ -207,9 +222,7 @@ export default {
         @keydown.down="value=''"
       >
     </v-row>
-    <v-row
-      class="px-3 py-1 justify-center item-row flex-nowrap"
-    >
+    <v-row class="px-3 py-1 justify-center item-row flex-nowrap">
       <v-spacer v-if="!isTrack" />
       <template v-if="selected">
         <tooltip-btn
@@ -221,7 +234,7 @@ export default {
 
         <tooltip-btn
           v-if="isTrack"
-          :disabled="!track.canSplit(frame.value)"
+          :disabled="!track.canSplit(frame)"
           icon="mdi-call-split"
           tooltip-text="Split Track"
           @click="$emit('split')"

client/app/api/viame.service.ts
@@ -9,6 +9,7 @@ interface GirderModel {
 interface Attribute {
   belongs: 'track' | 'detection';
   datatype: 'text' | 'number' | 'boolean';
+  values?: string[];
   name: string;
   _id: string;
 }

client/app/components/AttributesPanel.vue
@@ -1,13 +1,15 @@
 <script lang="ts">
-import { Ref } from '@vue/composition-api';
-import Vue, { PropType } from 'vue';
+import {
+  computed, defineComponent, onBeforeMount, ref,
+} from '@vue/composition-api';
 
+import { useSelectedTrackId, useFrame, useTrackMap } from 'vue-media-annotator/provides';
 import Track, { TrackId, Feature } from 'vue-media-annotator/track';
 
-import { getAttributes, Attribute } from 'app/api/viame.service';
+import { Attribute, getAttributes } from 'app/api/viame.service';
 import AttributeInput from 'app/components/AttributeInput.vue';
 
-function getTrack(trackMap: Map<TrackId, Track>, trackId: TrackId): Track {
+function getTrack(trackMap: Readonly<Map<TrackId, Track>>, trackId: TrackId): Track {
   const track = trackMap.get(trackId);
   if (track === undefined) {
     throw new Error(`Track ${trackId} missing from map`);
@@ -15,91 +17,73 @@ function getTrack(trackMap: Map<TrackId, Track>, trackId: TrackId): Track {
   return track;
 }
 
-export default Vue.extend({
+export default defineComponent({
   components: {
     AttributeInput,
   },
 
-  props: {
-    trackMap: {
-      type: Map as PropType<Map<TrackId, Track>>,
-      required: true,
-    },
-    selectedTrackId: {
-      type: Object as PropType<Ref<TrackId>>,
-      required: true,
-    },
-    frame: {
-      type: Object as PropType<Ref<number>>,
-      required: true,
-    },
-  },
-
-  data: () => ({
-    attributes: [] as Attribute[],
-  }),
-
-  computed: {
-    trackAttributes() {
-      if (!this.attributes) {
-        return [];
-      }
-      return this.attributes.filter(
-        (attribute) => attribute.belongs === 'track',
-      );
-    },
-    detectionAttributes() {
-      if (!this.attributes) {
-        return [];
-      }
-      return this.attributes.filter(
-        (attribute) => attribute.belongs === 'detection',
-      );
-    },
-    selectedTrack() {
-      const trackId = this.selectedTrackId.value;
-      if (trackId !== null) {
-        const track = getTrack(this.trackMap, trackId);
-        return {
-          trackId: track.trackId,
-          confidencePairs: track.confidencePairs,
-          attributes: track.attributes,
-        };
+  setup() {
+    const attributes = ref([] as Attribute[]);
+    const trackMap = useTrackMap();
+    const frameRef = useFrame();
+    const selectedTrackIdRef = useSelectedTrackId();
+    const trackAttributes = computed(() => attributes.value.filter(
+      (a) => a.belongs === 'track',
+    ));
+    const detectionAttributes = computed(() => attributes.value.filter(
+      (a) => a.belongs === 'detection',
+    ));
+    const selectedTrack = computed(() => {
+      if (selectedTrackIdRef.value !== null) {
+        return getTrack(trackMap, selectedTrackIdRef.value);
       }
       return null;
-    },
-    selectedDetection() {
-      const trackId = this.selectedTrackId.value;
-      const frame = this.frame.value;
-      if (trackId !== null) {
-        const track = getTrack(this.trackMap, trackId);
-        const [feature] = track.getFeature(frame);
-        return feature;
+    });
+    const selectedDetection = computed(() => {
+      const t = selectedTrack.value;
+      if (t !== null) {
+        const [real] = t.getFeature(frameRef.value);
+        return real;
       }
       return null;
-    },
-  },
+    });
 
-  async created() {
-    this.attributes = await getAttributes();
-  },
-
-  methods: {
-    updateTrackAttribute(
-      trackId: TrackId,
+    function updateTrackAttribute(
+      trackId: TrackId | null,
       { name, value }: { name: string; value: unknown },
     ) {
-      const track = getTrack(this.trackMap, trackId);
+      if (trackId === null) return;
+      const track = getTrack(trackMap, trackId);
       track.setAttribute(name, value);
-    },
-    updateFeatureAttribute(
-      trackId: TrackId,
-      oldFeature: Feature,
+    }
+
+    function updateFeatureAttribute(
+      trackId: TrackId | null,
+      oldFeature: Feature | null,
       { name, value }: { name: string; value: unknown },
     ) {
-      const track = getTrack(this.trackMap, trackId);
+      if (trackId === null) return;
+      if (oldFeature === null) return;
+      const track = getTrack(trackMap, trackId);
       track.setFeatureAttribute(oldFeature.frame, name, value);
-    },
+    }
+
+    onBeforeMount(async () => {
+      attributes.value = await getAttributes();
+    });
+
+    return {
+      selectedTrackIdRef,
+      /* Attributes */
+      detectionAttributes,
+      trackAttributes,
+      /* Selected */
+      selectedDetection,
+      selectedTrack,
+      /* Update functions */
+      updateFeatureAttribute,
+      updateTrackAttribute,
+    };
   },
 });
 </script>
@@ -154,11 +138,11 @@ export default Vue.extend({
             :name="attribute.name"
             :values="attribute.values ? attribute.values : null"
             :value="
-              selectedTrack.trackAttributes
-                ? selectedTrack.trackAttributes[attribute.name]
+              selectedTrack
+                ? selectedTrack.attributes[attribute.name]
                 : undefined
             "
-            @change="updateTrackAttribute(selectedTrackId.value, $event)"
+            @change="updateTrackAttribute(selectedTrackIdRef, $event)"
           />
         </div>
       </template>
@@ -177,9 +161,6 @@ export default Vue.extend({
           style="line-height:22px;"
         >
           <div>Frame: {{ selectedDetection.frame }}</div>
-          <div v-if="selectedDetection.confidence">
-            Confidence: {{ selectedDetection.confidence }}
-          </div>
           <div v-if="selectedDetection.fishLength">
             Fish length: {{ selectedDetection.fishLength }}
           </div>
@@ -190,11 +171,11 @@ export default Vue.extend({
             :name="attribute.name"
             :values="attribute.values ? attribute.values : null"
             :value="
-              selectedDetection.attributes
+              (selectedDetection && selectedDetection.attributes)
                 ? selectedDetection.attributes[attribute.name]
                 : undefined
             "
-            @change="updateFeatureAttribute(selectedTrackId.value, selectedDetection, $event)"
+            @change="updateFeatureAttribute(selectedTrackIdRef, selectedDetection, $event)"
           />
         </div>
       </template>

client/app/components/CreationMode.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 import Vue, { PropType } from 'vue';
-import { Ref } from '@vue/composition-api';
 import { cloneDeep } from 'lodash';
 import { NewTrackSettings } from 'app/use/useSettings';
 
@@ -9,11 +8,11 @@ export default Vue.extend({
 
   props: {
     allTypes: {
-      type: Object as PropType<Ref<Array<string>>>,
+      type: Array as PropType<Array<string>>,
       required: true,
     },
     newTrackSettings: {
-      type: Object as PropType<Ref<NewTrackSettings>>,
+      type: Object as PropType<NewTrackSettings>,
       required: true,
     },
   },
@@ -35,14 +34,14 @@ export default Vue.extend({
   computed: {
     typeList() {
       // Add unknown as the default type to the typeList
-      return ['unknown'].concat(this.allTypes.value);
+      return ['unknown'].concat(this.allTypes);
     },
   },
   methods: {
     /** Reduces the number of update functions utilizing Vuex by indicating the target type */
     saveTypeSettings(event: 'Track' | 'Detection', target: 'mode' | 'type') {
       // Copy the newTrackSettings for modification
-      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings.value);
+      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings);
       copy[target] = event; // Modify the value
       this.$emit('update-new-track-settings', copy);
     },
@@ -52,14 +51,14 @@ export default Vue.extend({
      */
     saveTrackSubSettings(event: true | null, target: 'autoAdvanceFrame' | 'interpolate') {
       // Copy the newTrackSettings for modification
-      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings.value);
+      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings);
       const modeSettings = copy.modeSettings.Track;
       modeSettings[target] = !!event; // Modify the value
       this.$emit('update-new-track-settings', copy);
     },
     saveDetectionSubSettings(event: true | null, target: 'continuous') {
       // Copy the newTrackSettings for modification
-      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings.value);
+      const copy: NewTrackSettings = cloneDeep(this.newTrackSettings);
       const modeSettings = copy.modeSettings.Detection;
       modeSettings[target] = !!event; // Modify the value
       this.$emit('update-new-track-settings', copy);
@@ -89,7 +88,7 @@ export default Vue.extend({
         </v-col>
         <v-col>
           <v-select
-            v-model="newTrackSettings.value.mode"
+            v-model="newTrackSettings.mode"
             class="ml-0 pa-0"
             x-small
             :items="modes"
@@ -111,7 +110,7 @@ export default Vue.extend({
                 mdi-help
               </v-icon>
             </template>
-            <span>{{ help.mode[newTrackSettings.value.mode] }}</span>
+            <span>{{ help.mode[newTrackSettings.mode] }}</span>
           </v-tooltip>
         </v-col>
       </v-row>
@@ -127,7 +126,7 @@ export default Vue.extend({
         </v-col>
         <v-col>
           <v-combobox
-            :value="newTrackSettings.value.type"
+            :value="newTrackSettings.type"
             class="ml-0 pa-0"
             x-small
             :items="typeList"
@@ -154,12 +153,12 @@ export default Vue.extend({
         </v-col>
       </v-row>
       <v-row
-        v-if="newTrackSettings.value.mode==='Track'"
+        v-if="newTrackSettings.mode==='Track'"
       >
         <v-col class="py-1">
           <v-switch
             :input-value="
-              newTrackSettings.value.modeSettings.Track.autoAdvanceFrame"
+              newTrackSettings.modeSettings.Track.autoAdvanceFrame"
             class="my-0 ml-1 pt-0"
             dense
             label="Advance Frame"
@@ -189,7 +188,7 @@ export default Vue.extend({
         <v-col class="py-1">
           <v-switch
             :input-value="
-              newTrackSettings.value.modeSettings.Track.interpolate"
+              newTrackSettings.modeSettings.Track.interpolate"
             class="my-0 ml-1 pt-0"
             dense
             label="Interpolate"
@@ -218,11 +217,11 @@ export default Vue.extend({
         </v-col>
       </v-row>
       <v-row
-        v-if="newTrackSettings.value.mode==='Detection'"
+        v-if="newTrackSettings.mode==='Detection'"
       >
         <v-col>
           <v-switch
-            :input-value="newTrackSettings.value.modeSettings.Detection.continuous"
+            :input-value="newTrackSettings.modeSettings.Detection.continuous"
             class="my-0 ml-1 pt-0"
             dense
             label="Continuous"

client/app/components/DeleteControls.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 import Vue, { PropType } from 'vue';
-import { Ref } from '@vue/composition-api';
 import { EditAnnotationTypes } from 'vue-media-annotator/layers/EditAnnotationLayer';
 
 export default Vue.extend({
@@ -8,21 +7,21 @@ export default Vue.extend({
 
   props: {
     selectedFeatureHandle: {
-      type: Object as PropType<Ref<number>>,
+      type: Number,
       required: true,
     },
     editingMode: {
-      type: Object as PropType<Ref<EditAnnotationTypes | boolean>>,
+      type: [String, Boolean] as PropType<EditAnnotationTypes | boolean>,
       required: true,
     },
   },
 
   computed: {
     disabled(): boolean {
-      if (this.selectedFeatureHandle.value < 0 && this.editingMode.value === false) {
+      if (this.selectedFeatureHandle < 0 && this.editingMode === false) {
         return true;
       }
-      if (this.editingMode.value === 'rectangle') {
+      if (this.editingMode === 'rectangle') {
         return true; // deleting rectangle is unsupported
       }
       return false;
@@ -34,7 +33,7 @@ export default Vue.extend({
       if (this.disabled) {
         throw new Error('Cannot delete while disabled!');
       }
-      if (this.selectedFeatureHandle.value >= 0) {
+      if (this.selectedFeatureHandle >= 0) {
         this.$emit('delete-point');
       } else {
         this.$emit('delete-annotation');
@@ -53,11 +52,11 @@ export default Vue.extend({
       @click="deleteSelected"
     >
       <pre class="mr-1 text-body-2">del</pre>
-      <span v-if="selectedFeatureHandle.value >= 0">
-        point {{ selectedFeatureHandle.value }}
+      <span v-if="selectedFeatureHandle >= 0">
+        point {{ selectedFeatureHandle }}
       </span>
-      <span v-else-if="editingMode.value">
-        {{ editingMode.value }}
+      <span v-else-if="editingMode">
+        {{ editingMode }}
       </span>
       <span v-else>unselected</span>
       <v-icon

client/app/components/EditorMenu.vue
@@ -1,7 +1,6 @@
 <script lang="ts">
 import { flatten } from 'lodash';
 import Vue, { PropType } from 'vue';
-import { Ref } from '@vue/composition-api';
 
 import { Mousetrap } from 'vue-media-annotator/types';
 import { EditAnnotationTypes } from 'vue-media-annotator/layers/EditAnnotationLayer';
@@ -20,15 +19,15 @@ export default Vue.extend({
   name: 'EditorMenu',
   props: {
     editingTrack: {
-      type: Object as PropType<Ref<boolean>>,
+      type: Boolean,
       required: true,
     },
     visibleModes: {
-      type: Object as PropType<Ref<EditAnnotationTypes[]>>,
+      type: Array as PropType<EditAnnotationTypes[]>,
       required: true,
     },
     editingMode: {
-      type: Object as PropType<Ref<EditAnnotationTypes>>,
+      type: [String, Boolean] as PropType<false | EditAnnotationTypes>,
       required: true,
     },
     recipes: {
@@ -39,12 +38,12 @@ export default Vue.extend({
 
   computed: {
     editButtons(): ButtonData[] {
-      const em = this.editingMode.value;
+      const em = this.editingMode;
       return [
         {
           id: 'rectangle',
           icon: 'mdi-vector-square',
-          active: this.editingTrack.value && em === 'rectangle',
+          active: this.editingTrack && em === 'rectangle',
           mousetrap: [{
             bind: '1',
             handler: () => {
@@ -59,7 +58,7 @@ export default Vue.extend({
         ...this.recipes.filter((r) => r.toggleable.value).map((r, i) => ({
           id: r.name,
           icon: r.icon.value || 'mdi-pencil',
-          active: this.editingTrack.value && r.active.value,
+          active: this.editingTrack && r.active.value,
           click: () => r.activate(),
           mousetrap: [
             {
@@ -104,17 +103,17 @@ export default Vue.extend({
 
   methods: {
     isVisible(mode: EditAnnotationTypes) {
-      return this.visibleModes.value.includes(mode);
+      return this.visibleModes.includes(mode);
     },
 
     toggleVisible(mode: EditAnnotationTypes) {
       if (this.isVisible(mode)) {
         this.$emit('set-annotation-state', {
-          visible: this.visibleModes.value.filter((m) => m !== mode),
+          visible: this.visibleModes.filter((m) => m !== mode),
         });
       } else {
         this.$emit('set-annotation-state', {
-          visible: this.visibleModes.value.concat([mode]),
+          visible: this.visibleModes.concat([mode]),
         });
       }
     },
@@ -150,7 +149,7 @@ export default Vue.extend({
       <span
         :class="[
           'ml-8', 'mr-1', 'px-3', 'py-1',
-          'modechip', editingTrack.value ? 'primary' : ''
+          'modechip', editingTrack ? 'primary' : ''
         ]"
       >
         <v-icon class="pr-1">

client/app/use/useGirderDataset.ts
@@ -14,7 +14,7 @@ const defaultFrameRate = 30;
 interface VIAMEDataset extends GirderModel {
   meta: {
     type: 'video' | 'image-sequence';
-    fps: number;
+    fps: number | string;
     customTypeStyling?: Record<string, CustomStyle>;
     confidenceFilters?: Record<string, number>;
   };
@@ -29,8 +29,18 @@ export default function useGirderDataset() {
   const dataset = ref(null as VIAMEDataset | null);
   const imageData = ref([] as FrameImage[]);
   const videoUrl = ref('');
-  const frameRate = computed(() => (dataset.value && dataset.value.meta.fps as number)
-    || defaultFrameRate);
+  const frameRate = computed(() => {
+    if (dataset.value?.meta.fps) {
+      if (typeof dataset.value.meta.fps === 'string') {
+        const parsed = parseInt(dataset.value.meta.fps, 10);
+        if (Number.isNaN(parsed)) {
+          throw new Error(`Cannot parse fps=${dataset.value.meta.fps} as integer`);
+        }
+        return parsed;
+      }
+    }
+    return defaultFrameRate;
+  });
 
   const annotatorType = computed(() => {
     if (!dataset.value) {

client/app/views/Home.vue
@@ -144,7 +144,7 @@ export default {
 </script>
 
 <template>
-  <v-content>
+  <v-main>
     <NavigationBar />
     <v-progress-linear
       :indeterminate="loading"
@@ -236,7 +236,7 @@ export default {
         </v-col>
       </v-row>
     </v-container>
-  </v-content>
+  </v-main>
 </template>
 
 <style lang='scss'>

commit b3036b87529c2e75dc4d668b8a38b64a71662372

Fix regression introduced in #361

client/src/components/TrackItem.vue
@@ -129,7 +129,7 @@ export default defineComponent({
 
     function toggleKeyframe() {
       const f = feature.value;
-      if (f.real && f.isKeyframe) {
+      if (f.real && !f.isKeyframe) {
         props.track.setFeature({
           ...f.real,
           frame: frameRef.value,