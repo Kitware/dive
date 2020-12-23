5f830fa9717bd7b01477e9d38def43b66d4cde69
Brandon Davis <brandon.davis@kitware.com>
2020-12-23 09:35:20 -0500

    Add image name to UI

--- client/src/components/controls/Controls.vue
+++ client/src/components/controls/Controls.vue
@@ -60,90 +60,81 @@ export default defineComponent({
       { bind: 'd', handler: mediaController.prevFrame, disabled: $prompt.visible() },
     ]"
   >
-    <v-toolbar
-      height="80px"
-      flat
-    >
-      <v-container
-        fluid
-        class="pa-0"
-      >
-        <v-row>
-          <v-slider
-            hide-details
-            :min="0"
-            :max="mediaController.maxFrame.value"
-            :value="data.frame"
-            @start="dragHandler.start"
-            @end="dragHandler.end"
-            @input="input"
+    <v-card class="px-4 py-1">
+      <v-slider
+        hide-details
+        :min="0"
+        :max="mediaController.maxFrame.value"
+        :value="data.frame"
+        @start="dragHandler.start"
+        @end="dragHandler.end"
+        @input="input"
+      />
+      <v-row no-gutters>
+        <v-col class="pl-1 py-1 shrink">
+          <slot
+            justify="start"
+            name="timelineControls"
           />
-        </v-row>
-        <v-row
-          align-content="space-between"
-          dense
+        </v-col>
+        <v-col
+          class="py-1 shrink"
+          style="min-width: 100px;"
         >
-          <v-col class="pl-1 py-1">
-            <slot
-              justify="start"
-              name="timelineControls"
-            />
-          </v-col>
-          <v-col
-            align="center"
-            class="py-1 shrink"
-            style="min-width: 100px;"
+          <v-btn
+            icon
+            small
+            title="(d, left-arrow) previous frame"
+            @click="mediaController.prevFrame"
           >
-            <v-btn
-              icon
-              small
-              title="(d, left-arrow) previous frame"
-              @click="mediaController.prevFrame"
-            >
-              <v-icon>mdi-skip-previous</v-icon>
-            </v-btn>
-            <v-btn
-              v-if="!mediaController.playing.value"
-              icon
-              small
-              title="(space) Play"
-              @click="mediaController.play"
-            >
-              <v-icon>mdi-play</v-icon>
-            </v-btn>
-            <v-btn
-              v-else
-              icon
-              small
-              title="(space) Pause"
-              @click="mediaController.pause"
-            >
-              <v-icon>mdi-pause</v-icon>
-            </v-btn>
-            <v-btn
-              icon
-              small
-              title="(f, right-arrow) next frame"
-              @click="mediaController.nextFrame"
-            >
-              <v-icon>mdi-skip-next</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col
-            class="pl-1 py-1"
-            align="right"
+            <v-icon>mdi-skip-previous</v-icon>
+          </v-btn>
+          <v-btn
+            v-if="!mediaController.playing.value"
+            icon
+            small
+            title="(space) Play"
+            @click="mediaController.play"
           >
-            <v-btn
-              icon
-              small
-              title="(r)eset pan and zoom"
-              @click="mediaController.resetZoom"
-            >
-              <v-icon>mdi-image-filter-center-focus</v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-toolbar>
+            <v-icon>mdi-play</v-icon>
+          </v-btn>
+          <v-btn
+            v-else
+            icon
+            small
+            title="(space) Pause"
+            @click="mediaController.pause"
+          >
+            <v-icon>mdi-pause</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            small
+            title="(f, right-arrow) next frame"
+            @click="mediaController.nextFrame"
+          >
+            <v-icon>mdi-skip-next</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col
+          class="pl-1 py-1 shrink"
+        >
+          <slot name="middle" />
+        </v-col>
+        <v-col
+          class="pl-1 py-1"
+          align="right"
+        >
+          <v-btn
+            icon
+            small
+            title="(r)eset pan and zoom"
+            @click="mediaController.resetZoom"
+          >
+            <v-icon>mdi-image-filter-center-focus</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
