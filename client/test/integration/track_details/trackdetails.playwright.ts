c911c429fc5cc7755200fa8c00c166bdfebb9557
BryonLewis <Bryon.Lewis@kitware.com>
2021-03-13 15:40:53 -0500

    adding attributes screenshots

--- client/test/integration/track_details/trackdetails.playwright.ts
+++ client/test/integration/track_details/trackdetails.playwright.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-irregular-whitespace */
 /* eslint-disable @typescript-eslint/camelcase */
 import {
   chromium, Browser, Page,
@@ -22,8 +23,8 @@ export default function runTrackDetailsTest() {
     await page.close();
   });
 
-  describe('uploading Data to the system', () => {
-    it('uploading Data', async () => {
+  describe('Track Details and Attributes', () => {
+    it('Generatomg Track Attributes', async () => {
       await login({ page });
       await page.click('text=Public');
       // assert.equal(page.url(), 'http://localhost:8080/#/folder/6046d09ca7e8106ce7a94483');
@@ -36,112 +37,147 @@ export default function runTrackDetailsTest() {
       // Wait for the elements to stop loading
       await page.waitForSelector('.loadingSpinner', { state: 'detached' });
 
-      await page.click('button:has-text("Track")');
+      // Screenshot - Path: ./test/screenshots/track_details/swapIcon.png
+      const _test_screenshots_track_details_swapIcon = await page.$('.swap-button');
+      if (_test_screenshots_track_details_swapIcon) {
+        await _test_screenshots_track_details_swapIcon.screenshot({
+          path: './test/screenshots/track_details/swapIcon.png',
+        });
+      }
+      await page.mouse.move(663, 347, { steps: 5 });
+      await page.mouse.click(667, 347);
+      await page.mouse.move(273, 140, { steps: 20 });
 
-      await page.waitForSelector('span:has-text("Editing").primary');
-
-      // Mouse-down at Position: 610,145
-      await page.mouse.move(610, 145, { steps: 20 });
-      await page.mouse.down();
-      // Mouse-up at Position: 748,247
-      await page.mouse.move(748, 247, { steps: 20 });
-      await page.mouse.up();
-      // Mouse-down at Position: 816,217
-      await page.mouse.move(816, 217, { steps: 20 });
-      await page.mouse.down();
-
-      // Click .swap-button
+      await page.waitForTimeout(3000);
+      await await page.screenshot({
+        clip: {
+          x: 0, y: 115, width: 496, height: 473,
+        },
+        path: './test/screenshots/track_details/goingToDetails.png',
+      });
       await page.click('.swap-button');
-      const test_screenshots_track_details_track_details = await page.$('text=Track Editor seriola pristipomoides_auricilla etelis_carbunculus unknown 5 Confi');
-      await test_screenshots_track_details_track_details?.screenshot({
-        path: './test/screenshots/track_details/track_details.png',
-      });
 
-      await page.click('button:has-text("Attribute")');
 
-      const test_screenshots_track_details_track_attributes_settings = await page.$('text=Attributes NameDatatypeText​Predefined values Delete Cancel Save');
-      await test_screenshots_track_details_track_attributes_settings?.screenshot({
-        path: './test/screenshots/track_details/track_attributes_settings.png',
-      });
-
-      // Click textarea
-      await page.click('textarea');
-      // Fill textarea
-      await page.fill('textarea', 'value1');
-      // Press Enter
-      await page.press('textarea', 'Enter');
-      // Fill textarea
-      await page.fill('textarea', 'value1\nvalue2\nvalue3');
-      // Press Enter
-      await page.press('textarea', 'Enter');
-
-      await page.click('button:has-text("Save")');
-
-      // Click text=NewTrackAttribute: value1 value2 value3 >> input[type="text"]
-      await page.click('text=NewTrackAttribute: value1 value2 value3 >> input[type="text"]');
-      // Fill text=NewTrackAttribute: value1 value2 value3 >> input[type="text"]
-      await page.fill('text=NewTrackAttribute: value1 value2 value3 >> input[type="text"]', 'value1');
-
-      await page.click('text=NewTrackAttribute: value1 value2 value3 >> button');
-      // Screenshot - Path: ./test/screenshots/track_details/track_attributes_settings_edit.png
-      const test_screenshots_track_details_track_attributes_settings_edit = await page.$('text=Attributes Changes to Attribute Datatypes or Names do not effect currently set a');
-      await test_screenshots_track_details_track_attributes_settings_edit?.screenshot({
-        path: './test/screenshots/track_details/track_attributes_settings_edit.png',
-      });
-
-      await page.click('button:has-text("Cancel")');
-
+      //SETUP FOR ATTRIBUTES
+      /*
+        TODO: After load csv attributes lands this can probably be removed
+        although we want the creation of new attributes
+      */
       // Click button:has-text("Attribute")
       await page.click('button:has-text("Attribute")');
+      // Click text=NameDatatypeText​Predefined values >> input[type="text"]
       await page.click('text=NameDatatypeText​Predefined values >> input[type="text"]');
-      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'BooleanAttribute');
+      // Fill text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'CompleteTrack');
+      // Click div[role="button"]:has-text("DatatypeText")
+      await page.click('div[role="button"]:has-text("DatatypeText")');
+      // Click text=Boolean
+      await page.click('text=Boolean');
+      // Click button:has-text("Save")
+      await page.click('button:has-text("Save")');
+      // Click text=Detection Attributes: Attribute >> button
+      await page.click('text=Detection Attributes: Attribute >> button');
+      // Click text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.click('text=NameDatatypeText​Predefined values >> input[type="text"]');
+      // Fill text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'swimming');
       // Click div[role="button"]:has-text("DatatypeText")
       await page.click('div[role="button"]:has-text("DatatypeText")');
       // Click div[role="option"] div:has-text("Boolean")
       await page.click('div[role="option"] div:has-text("Boolean")');
       // Click button:has-text("Save")
       await page.click('button:has-text("Save")');
-
       // Click button:has-text("Attribute")
       await page.click('button:has-text("Attribute")');
+      // Click text=NameDatatypeText​Predefined values >> input[type="text"]
       await page.click('text=NameDatatypeText​Predefined values >> input[type="text"]');
-      await page.click('text=NameDatatypeText​Predefined values >> input[type="text"]');
-      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'NumberAttribute');
+      // Fill text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'fishLength');
       // Click div[role="button"]:has-text("DatatypeText")
       await page.click('div[role="button"]:has-text("DatatypeText")');
-      // Click text=Number
+      // Click #list-item-523-1 div:has-text("Number")
       await page.click('text=Number');
       // Click button:has-text("Save")
       await page.click('button:has-text("Save")');
+      // Click text=Detection Attributes: Attribute >> button
+      await page.click('text=Detection Attributes: Attribute >> button');
+      // Click text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.click('text=NameDatatypeText​Predefined values >> input[type="text"]');
+      // Fill text=NameDatatypeText​Predefined values >> input[type="text"]
+      await page.fill('text=NameDatatypeText​Predefined values >> input[type="text"]', 'eating');
+      // Click div[role="button"]:has-text("DatatypeText")
+      await page.click('div[role="button"]:has-text("DatatypeText")');
+      // Click text=Boolean
+      await page.click('text=Boolean');
+      // Click button:has-text("Save")
+      await page.click('button:has-text("Save")');
 
-      // Select false
-      await page.selectOption('select', 'false');
+      // Click text=CompleteTrack: true false >> :nth-match(div, 3)
+      await page.click('text=CompleteTrack: true false >> :nth-match(div, 3)');
+      // Select true
+      await page.selectOption('select', 'true');
       // Click input[type="number"]
       await page.click('input[type="number"]');
       // Fill input[type="number"]
-      await page.fill('input[type="number"]', '2345');
+      await page.fill('input[type="number"]', '20');
+      // Select true
+      await page.selectOption('text=swimming: true false >> select', 'true');
+      // Click .px-4 .row div:nth-child(2) button:nth-child(3)
+      await page.click('.px-4 .row div:nth-child(2) button:nth-child(3)');
+      // Select false
+      await page.selectOption('text=swimming: true false >> select', 'false');
+      // Click .px-4 .row div:nth-child(2) button
+      await page.click('.px-4 .row div:nth-child(2) button');
 
-      // Click text=Detection Attributes: Attribute >> button
-      await page.click('text=Detection Attributes: Attribute >> button');
-      // Click div[role="button"]:has-text("DatatypeText")
-      await page.click('div[role="button"]:has-text("DatatypeText")');
-      // Click #list-item-557-1 div:has-text("Number")
-      await page.click('div[role="option"]  div:has-text("Number")');
-      // Click button:has-text("Save")
-      await page.click('button:has-text("Save")');
-      // Click text=Detection Attributes: Attribute Frame: 0 NewDetectionAttribute: >> input[type="number"]
-      await page.click('text=Detection Attributes: Attribute Frame: 0 NewDetectionAttribute: >> input[type="number"]');
-      // Fill text=Detection Attributes: Attribute Frame: 0 NewDetectionAttribute: >> input[type="number"]
-      await page.fill('text=Detection Attributes: Attribute Frame: 0 NewDetectionAttribute: >> input[type="number"]', '32432');
-      // Click text=Track Editor seriola pristipomoides_auricilla etelis_carbunculus unknown 5 Confi
-      await page.click('text=Track Editor seriola pristipomoides_auricilla etelis_carbunculus unknown 5 Confi');
-      // Click text=NewTrackAttribute: value1 value2 value3 >> button
-      await page.click('text=NewTrackAttribute: value1 value2 value3 >> button');
-      // Click button:has-text("Delete")
-      await page.click('button:has-text("Delete")');
-      // Click button:has-text("Confirm")
-      await page.click('button:has-text("Confirm")');
-      await page.pause();
-    }, 120000);
+      // Screenshot - Path: ./test/screenshots/track_details/trackDetailsFull.png
+      const _test_screenshots_track_details_trackDetailsFul = await page.$('text=Track Editor seriola pristipomoides_auricilla etelis_carbunculus 1 Confidence Pa');
+      if (_test_screenshots_track_details_trackDetailsFul) {
+        await _test_screenshots_track_details_trackDetailsFul.screenshot({
+          path: './test/screenshots/track_details/trackDetailsFull.png',
+        });
+      }
+      // Screenshot - Path: ./test/screenshots/track_details/eyeIcon.png
+      const _test_screenshots_track_details_eyeIcon = await page.$('text=Track Attributes: Attribute >> :nth-match(button, 2)');
+      if (_test_screenshots_track_details_eyeIcon) {
+        await _test_screenshots_track_details_eyeIcon.screenshot({
+          path: './test/screenshots/track_details/eyeIcon.png',
+        });
+      }
+      // Screenshot - Path: ./test/screenshots/track_details/settingsIcon.png
+      const _test_screenshots_track_details_settingsIcon = await page.$('text=CompleteTrack: true false >> button');
+      if (_test_screenshots_track_details_settingsIcon) {
+        await _test_screenshots_track_details_settingsIcon.screenshot({
+          path: './test/screenshots/track_details/settingsIcon.png',
+        });
+      }
+
+      // Screenshot - Path: ./test/screenshots/track_details/AttributeButton.png
+      const _test_screenshots_track_details_AttributeButton = await page.$('button:has-text("Attribute")');
+      if (_test_screenshots_track_details_AttributeButton) {
+        await _test_screenshots_track_details_AttributeButton.screenshot({
+          path: './test/screenshots/track_details/AttributeButton.png',
+        });
+      }
+      await page.click('button:has-text("Attribute")');
+
+      const _test_screenshots_track_details_newAttribute = await page.$('text=Attributes NameDatatypeText​Predefined values Delete Cancel Save');
+      if (_test_screenshots_track_details_newAttribute) {
+        await _test_screenshots_track_details_newAttribute.screenshot({
+          path: './test/screenshots/track_details/newAttribute.png',
+        });
+      }
+
+      // Click button:has-text("Cancel")
+      await page.click('button:has-text("Cancel")');
+      // Click text=CompleteTrack: true false >> button
+      await page.click('text=CompleteTrack: true false >> button');
+      // Screenshot - Path: ./test/screenshots/track_details/editAttribute.png
+      const _test_screenshots_track_details_editAttribute = await page.$('text=Attributes Changes to Attribute Datatypes or Names do not effect currently set a');
+      if (_test_screenshots_track_details_editAttribute) {
+        await _test_screenshots_track_details_editAttribute.screenshot({
+          path: './test/screenshots/track_details/editAttribute.png',
+        });
+      }
+    }, 220000);
   });
 }
