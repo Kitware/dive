09eaa3f9653f29d54f61fb33a7c7e786fd62c3a2
Brandon Davis <brandon.davis@kitware.com>
2021-04-12 12:36:11 -0400

    Add girder search (#688)

--- client/platform/web-girder/store/Location.ts
+++ client/platform/web-girder/store/Location.ts
@@ -1,6 +1,8 @@
 import type { Module } from 'vuex';
 import type { GirderModel } from '@girder/components/src';
+import { getPathFromLocation } from 'platform/web-girder/utils';
 import type { LocationState, RootState } from './types';
+import router from '../router';
 
 const locationModule: Module<LocationState, RootState> = {
   namespaced: true,
@@ -10,6 +12,15 @@ const locationModule: Module<LocationState, RootState> = {
   mutations: {
     setLocation(state, location: GirderModel) {
       state.location = location;
+    },
+  },
+  actions: {
+    route({ commit }, location: GirderModel) {
+      const newPath = getPathFromLocation(location);
+      if (newPath !== router.currentRoute.path) {
+        router.push(newPath);
+      }
+      commit('setLocation', location);
     },
   },
 };
--- client/platform/web-girder/store/index.ts
+++ client/platform/web-girder/store/index.ts
@@ -1,16 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import router from '../router';
 import { RootState } from './types';
 import Location from './Location';
 import Dataset from './Dataset';
 import Brand from './Brand';
 
+
 Vue.use(Vuex);
 
-export default new Vuex.Store<RootState>({
+const store = new Vuex.Store<RootState>({
   modules: {
     Brand,
     Location,
     Dataset,
   },
 });
+
+/* Keep location state up to date with current route */
+router.beforeEach((to, from, next) => {
+  if (to.name === 'home') {
+    store.commit('Location/setLocation', to.params);
+  }
+  next();
+});
+
+export default store;
