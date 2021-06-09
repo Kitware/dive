0be2801ea9aab18d2f158f0ca8fd6ac4d19490bd
Farhan Ahmed <farhan.ahmed@kitware.com>
2021-06-09 14:56:17 -0400

    Created new prompt service using composition api

--- client/dive-common/vue-utilities/prompt-service/index.js
+++ client/dive-common/vue-utilities/prompt-service/index.js
@@ -1,14 +1,87 @@
 /* disabled this rule for Vue.prototype.FOO = */
 /* eslint-disable no-param-reassign,func-names */
 
+import Vue2 from 'vue';
+import {
+  provide, inject,
+} from '@vue/composition-api';
 import Prompt from './Prompt.vue';
+
+class PromptService {
+  constructor() {
+    const PromptComponent = Vue2.extend(Prompt);
+    const component = new PromptComponent();
+    this.component = component;
+  }
+
+  show({
+    title,
+    text,
+    positiveButton = 'Confirm',
+    negativeButton = 'Cancel',
+    confirm = false,
+  } = {}) {
+    let resolve;
+    const p = new Promise((_resolve) => {
+      resolve = _resolve;
+    });
+    function set(component) {
+      component.title = title;
+      component.text = text;
+      component.positiveButton = positiveButton;
+      component.negativeButton = negativeButton;
+      component.confirm = confirm;
+      component.resolve = resolve;
+      component.show = true;
+    }
+    if (!this.component.$data.show) {
+      set(this.component);
+    } else {
+      const unwatch = this.component.$watch('show', () => {
+        unwatch();
+        set(this.component);
+      });
+    }
+    return p;
+  }
+
+  visible() {
+    return this.component.show;
+  }
+
+  invisible() {
+    return !this.component.show;
+  }
+
+  hide() {
+    this.component.show = false;
+  }
+}
+
+const promptSymbol = Symbol('prompt-service');
+
+export function providePrompt() {
+  const promptService = new PromptService();
+  Vue2.prototype.$promptAttach = function () {
+    const div = document.createElement('div');
+    this.$el.appendChild(div);
+    promptService.component.$mount(div);
+    return this;
+  };
+  provide(promptSymbol, promptService);
+}
+
+export function usePrompt() {
+  const prompt = inject(promptSymbol);
+  return prompt;
+}
 
 export default function (vuetify) {
   return function install(Vue) {
     Prompt.vuetify = vuetify;
     const PromptComponent = Vue.extend(Prompt);
     const component = new PromptComponent();
-    Vue.prototype.$promptAttach = function () {
+    Vue.prototype.$promptAttachOld = function () {
       const div = document.createElement('div');
       this.$el.appendChild(div);
       component.$mount(div);
