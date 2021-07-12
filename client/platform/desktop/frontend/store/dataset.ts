e8a5b53ec4ea58c68c0f000301a87502aa1b3f52
Bryon Lewis <Bryon.Lewis@kitware.com>
2021-07-12 11:14:22 -0400

    fixing the proper issue

--- client/platform/desktop/frontend/store/dataset.ts
+++ client/platform/desktop/frontend/store/dataset.ts
@@ -63,8 +63,6 @@ function load(): JsonMetaCache[] {
         maybeArr.forEach((meta: JsonMetaCache) => (
           Vue.set(datasets.value, meta.id, hydrateJsonMetaCacheValue(meta))
         ));
-        const values = Object.values(datasets.value);
-        window.localStorage.setItem(RecentsKey, JSON.stringify(values));
         return maybeArr;
       }
     }
