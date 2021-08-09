5e9c3b4e3c40505c9e8ddd12f4033f96c40288d5
Bryon Lewis <Bryon.Lewis@kitware.com>
2021-08-09 10:53:53 -0400

    updated test

--- client/platform/desktop/backend/native/common.spec.ts
+++ client/platform/desktop/backend/native/common.spec.ts
@@ -391,12 +391,20 @@ describe('native.common', () => {
     expect(payload.jsonMeta.originalBasePath).toBe('/home/user/data/imageSuccess');
   });
 
-  it('import with CSV annotations', async () => {
+  it('import with CSV annotations without specifying track file', async () => {
     const payload = await common.beginMediaImport(settings, '/home/user/data/imageSuccessWithAnnotations', checkMedia);
     payload.jsonMeta.fps = 12; // simulate user specify FPS action
     await common.finalizeMediaImport(settings, payload, updater, convertMedia);
     const meta = await common.loadMetadata(settings, payload.jsonMeta.id, urlMapper);
-    expect(meta.fps).toBe(32);
+    expect(meta.fps).toBe(12);
+  });
+  it('import with CSV annotations with specifying track file', async () => {
+    const payload = await common.beginMediaImport(settings, '/home/user/data/imageSuccessWithAnnotations', checkMedia);
+    payload.trackFileAbsPath = '/home/user/data/imageSuccessWithAnnotations/file1.csv';
+    payload.jsonMeta.fps = 12; // simulate user specify FPS action
+    await common.finalizeMediaImport(settings, payload, updater, convertMedia);
+    const meta = await common.loadMetadata(settings, payload.jsonMeta.id, urlMapper);
+    expect(meta.fps).toBe(12);
   });
 
   it('import with user selected FPS > originalFPS', async () => {
