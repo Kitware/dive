2fb75d1ccb7da06159cb5e15bdf37d438f9e754d
BryonLewis <61746913+BryonLewis@users.noreply.github.com>
2021-12-01 13:24:07 -0500

    ToolTip overflow Fix and positioning (#1054)
    * tooltip fixes
    
    * updated displaying code for tooltip
    
    * removing extra code
    
    * minor tweaks and comments
    
    * filter guarding

--- client/src/layers/UILayers/UILayer.ts
+++ client/src/layers/UILayers/UILayer.ts
@@ -1,16 +1,23 @@
 import { createApp } from '@vue/composition-api';
+import geo, { GeoEvent } from 'geojs';
 import { MediaController } from '../../components/annotators/mediaControllerType';
 
 
 interface WidgetPosition {
-  x: string | number;
-  y: string | number;
+  x: number;
+  y: number;
 }
+
 export interface DOMWidget {
   canvas: () => HTMLElement;
   isInViewport: () => boolean;
-  position: (pos: WidgetPosition) => WidgetPosition;
+  position: (pos: WidgetPosition) => void;
+  toolTip: boolean; // Tooltip enabled or disabled
+  toolTipOffset: {x: number; y: number}; // Offset from Cursor Position
+  lastMousePos: WidgetPosition; //Last Mouse position for zooming adjustments
 }
+
+
 /**
  * UILayer provides a way to add Reactive VUE DOM Widgets to GeoJS
  * These widgets are created under their own Vue App and can't rely on parent elements
@@ -31,18 +38,64 @@ export default class UILayer {
       this.annotator = annotator;
       this.widgets = {};
       this.uiLayer = this.annotator.geoViewerRef.value.createLayer('ui');
+      this.uiLayer.geoOn(geo.event.mousemove, this.updateToolTipPositions);
+      this.uiLayer.geoOn(geo.event.zoom, this.zoomToolTipPosition);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    addDOMWidget(name: string, component: any, props: any, position = { x: 0, y: 0 }) {
+    updateWidgetToolTipPosition(mousePos: WidgetPosition, widget: DOMWidget) {
+      const tipOffset = widget.toolTipOffset;
+      const newOffset = this.uiLayer.map().gcsToDisplay(mousePos);
+      const finalOffset = this.uiLayer.map().displayToGcs(
+        {
+          x: newOffset.x + tipOffset.x, y: newOffset.y + tipOffset.y,
+        },
+      );
+      widget.position(finalOffset);
+    }
+
+    zoomToolTipPosition = () => {
+      Object.keys(this.widgets).forEach((name) => {
+        if (this.widgets[name].toolTip) {
+          const mousePos = this.widgets[name].lastMousePos;
+          this.updateWidgetToolTipPosition(mousePos, this.widgets[name]);
+        }
+      });
+    };
+
+    updateToolTipPositions = (evt: GeoEvent) => {
+      const mousePos = evt.geo;
+      Object.keys(this.widgets).forEach((name) => {
+        if (this.widgets[name].toolTip) {
+          this.updateWidgetToolTipPosition(mousePos, this.widgets[name]);
+          this.widgets[name].lastMousePos = mousePos;
+        }
+      });
+    };
+
+
+    addDOMWidget(
+      name: string,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      component: any, props: any,
+      position = { x: 0, y: 0 },
+    ) {
       const widget: DOMWidget = this.uiLayer.createWidget('dom', { position });
       widget.canvas().setAttribute('id', name);
       const parent = widget.canvas();
       const div = document.createElement('div');
       const element = parent.appendChild(div);
       createApp(component, props).mount(element);
+      widget.toolTipOffset = position;
+      widget.toolTip = false;
+      widget.lastMousePos = position;
       this.widgets[name] = widget;
-
       return widget;
+    }
+
+    // Toggle named widget toolTip On or Off
+    setToolTipWidget(name: string, on: boolean) {
+      if (this.widgets[name]) {
+        this.widgets[name].toolTip = on;
+      }
     }
 }
