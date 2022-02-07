2a00bcaf62e758a2276f5943233638e897c8fb77
BryonLewis <61746913+BryonLewis@users.noreply.github.com>
2022-02-07 16:37:31 -0500

    Fixing negative Coordinates in polygons  (#1145)
    * fixes-993
    
    * Adding tests and other regexes
    
    * unifying testing
    
    * adding in meta field to python
    
    * Adding in meta attributes test
    
    * fix image name for tests
    
    * fixing ordering of viame.spec.json
    
    * No keyframe or interpolate defaults (#1155)
    
    * adding clarification/documentation to unit test
    
    Co-authored-by: Brandon Davis <git@subdavis.com>

--- server/dive_utils/serializers/viame.py
+++ server/dive_utils/serializers/viame.py
@@ -118,14 +118,14 @@ def _parse_row(row: List[str]) -> Tuple[Dict, Dict, Dict, List]:
 
     for j in range(start, len(row)):
         # (kp) head x y
-        head_regex = re.match(r"^\(kp\) head ([0-9]+\.*[0-9]*) ([0-9]+\.*[0-9]*)", row[j])
+        head_regex = re.match(r"^\(kp\) head (-?[0-9]+\.*-?[0-9]*) (-?[0-9]+\.*-?[0-9]*)", row[j])
         if head_regex:
             point = [float(head_regex[1]), float(head_regex[2])]
             head_tail.append(point)
             create_geoJSONFeature(features, 'Point', point, 'head')
 
         # (kp) tail x y
-        tail_regex = re.match(r"^\(kp\) tail ([0-9]+\.*[0-9]*) ([0-9]+\.*[0-9]*)", row[j])
+        tail_regex = re.match(r"^\(kp\) tail (-?[0-9]+\.*-?[0-9]*) (-?[0-9]+\.*-?[0-9]*)", row[j])
         if tail_regex:
             point = [float(tail_regex[1]), float(tail_regex[2])]
             head_tail.append(point)
@@ -142,7 +142,7 @@ def _parse_row(row: List[str]) -> Tuple[Dict, Dict, Dict, List]:
             track_attributes[trk_regex[1]] = _deduceType(trk_regex[2])
 
         # (poly) x1 y1 x2 y2 ...
-        poly_regex = re.match(r"^(\(poly\)) ((?:[0-9]+\.*[0-9]*\s*)+)", row[j])
+        poly_regex = re.match(r"^(\(poly\)) ((?:-?[0-9]+\.*-?[0-9]*\s*)+)", row[j])
         if poly_regex:
             temp = [float(x) for x in poly_regex[2].split()]
             coords = list(zip(temp[::2], temp[1::2]))
