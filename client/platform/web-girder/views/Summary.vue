19befa2c4b61a681750ac9a6e4a72e00f307b202
Mary Salvi <mary.salvi@kitware.com>
2022-04-14 18:03:10 -0400

    add expandable data table for label summaries

--- client/platform/web-girder/views/Summary.vue
+++ client/platform/web-girder/views/Summary.vue
@@ -1,13 +1,69 @@
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref, reactive } from '@vue/composition-api';
+import type { DataOptions } from 'vuetify';
+import { GirderModel } from '@girder/components/src';
+import { getLabels } from '../api';
 
 export default defineComponent({
   setup() {
-    console.log('whatever');
+    const summaryList = ref();
+    const expanded = ref();
+    const headers = [
+      { text: 'Type', value: '_id' },
+      { text: 'Number of Datasets', value: 'datasets.length' },
+      { text: 'Number of Tracks', value: 'count' },
+      { text: '', value: 'data-table-expand' },
+    ];
+    const tableOptions = reactive({
+      page: 1,
+      sortBy: ['_id'],
+    } as DataOptions);
+    const updateList = async () => {
+      const response = await getLabels();
+
+      summaryList.value = response.data;
+    };
+
+    updateList();
+
+    return {
+      expanded,
+      summaryList,
+      headers,
+    };
   },
 });
 </script>
 
 <template>
-  <p>Summary placeholder</p>
+  <v-data-table
+    :headers="headers"
+    :items="summaryList"
+    :expanded.sync="expanded"
+    show-expand
+  >
+    <template
+      v-slot:expanded-item="{ headers, item }"
+    >
+      <td :colspan="headers.length">
+        <tr
+          v-for="dataset in item.datasets"
+          :key="dataset.id"
+        >
+          {{
+            dataset.name
+          }}
+          <v-btn
+            class="ml-2"
+            x-small
+            color="primary"
+            depressed
+            :to="{ name: 'viewer', params: { id: dataset.id } }"
+          >
+            Launch Annotator
+          </v-btn>
+        </tr>
+      </td>
+    </template>
+  </v-data-table>
 </template>
