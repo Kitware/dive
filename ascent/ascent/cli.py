0472dd67ab5f8f99ff92be9cc022ad50476d60d6
Mary Salvi <mary.salvi@kitware.com>
2023-09-25 18:06:56 -0400

    Upload annotations

--- ascent/ascent/cli.py
+++ ascent/ascent/cli.py
@@ -73,15 +73,36 @@ def upload_arguments(func):
     return func
 
 
+_upload_options = [
+    click.option(
+        "--parent-folder-id",
+        type=str,
+        help="The _id of girder folder that the new folder will be created in. Defaults to user root."
+    ),
+    click.option(
+        "--annotations-path",
+        type=click.Path(exists=True, readable=True, path_type=Path),
+        help="Local path for corresponding annotations file to be uploaded."
+
+    )
+
+]
+
+
+def upload_options(func):
+    for option in _upload_options:
+        func = option(func)
+    return func
+
+
 apiURL = "localhost"
 
 
 def login(api, port):
-    apiURL = api if api else "localhost"
-    local_port = port if port else 8010
+
     gc = girder_client.GirderClient(
-        apiURL,
-        port=local_port,
+        api,
+        port=port,
         apiRoot="girder/api/v1"
       )
     if not os.environ.get('DIVE_USER') or not os.environ.get('DIVE_PW'):
@@ -130,26 +151,48 @@ def upload():
 @upload_arguments
 @global_options
 @click.option(
-        "--parent-folder",
-        type=str,
-        help="The _id of girder folder that the new folder will be created in. Defaults to user root."
-    )
-def image_sequence(parent_folder, folder_name, path: Path, url, port):
+    "--annotations-included",
+    is_flag=True,
+    help="Annotations for the image set are in the same directory and should be uploaded"
+)
+@upload_options
+def image_sequence(
+    parent_folder_id,
+    folder_name,
+    path: Path,
+    url,
+    port,
+    annotations_included,
+    annotations_path: Path
+):
     """
     Create folder with FOLDER_NAME in PARENT_FOLDER and upload a set of images from PATH\n
 
     FOLDER_NAME is a unique name for the folder to be created \n
     PATH is the the local path for the images to be uploaded
     """
-    gc = login(url, port)
-    new_folder = create_folder(parent_folder, folder_name, gc)
+    apiURL = url if url else "localhost"
+    local_port = port if port else 8010
+    gc = login(apiURL, local_port)
+    new_folder = create_folder(parent_folder_id, folder_name, gc)
 
     if new_folder:
+        count = 1
+        images = len(list(path.iterdir()))
         for file in path.iterdir():
             if file.is_file():
+                if annotations_included:
+                    if file.suffix == '.json' or file.suffix == '.csv':
+                        images = images - 1
+                        gc.uploadFileToFolder(new_folder["_id"], file)
                 mimetype = mimetypes.guess_type(file.name)
                 if mimetype[0] in supportedImageTypes:
-                  gc.uploadFileToFolder(new_folder["_id"], file)
+                    click.echo(f"Uploading image {count} of {images}")
+                    gc.uploadFileToFolder(new_folder["_id"], file)
+                    count = count + 1
+
+        if annotations_path:
+            gc.uploadFileToFolder(new_folder["_id"], annotations_path)
         gc.addMetadataToFolder(
             new_folder["_id"],
             {
@@ -157,31 +200,36 @@ def image_sequence(parent_folder, folder_name, path: Path, url, port):
                 "fps": 1,
             },
         )
+        click.echo("Processing")
         gc.sendRestRequest(
             "POST",
             f"dive_rpc/postprocess/{new_folder['_id']}",
             data={"skipTranscoding": True},
         )
+        click.echo(
+            f"Dataset ready at: http://{apiURL}:{local_port}/#/viewer/{new_folder['_id']}"
+            )
 
 
 @upload.command()
 @upload_arguments
-@click.option(
-        "--parent-folder",
-        type=str,
-        help="The _id of girder folder that the new folder will be created in. Defaults to user root."
-    )
-def video(parent_folder, folder_name, path, url, port):
+@upload_options
+def video(parent_folder, folder_name, path, url, port, annotations_path: Path):
     """
     Create folder with FOLDER_NAME in PARENT_FOLDER and upload a video from PATH\n
 
     FOLDER_NAME is a unique name for the folder to be created \n
     PATH is the the local path for the video to be uploaded
     """
-    gc = login(url, port)
+    apiURL = url if url else "localhost"
+    local_port = port if port else 8010
+    gc = login(apiURL, local_port)
     new_folder = create_folder(parent_folder, folder_name, gc)
     if new_folder:
+        click.echo("Uploading video.")
         gc.uploadFileToFolder(new_folder["_id"], path)
+        if annotations_path:
+            gc.uploadFileToFolder(new_folder["_id"], annotations_path)
         gc.addMetadataToFolder(
             new_folder["_id"],
             {
@@ -189,29 +237,36 @@ def video(parent_folder, folder_name, path, url, port):
                 "fps": -1,
             },
         )
+        click.echo("Processing")
         gc.sendRestRequest(
             "POST",
             f"dive_rpc/postprocess/{new_folder['_id']}",
             data={"skipTranscoding": True},
         )
+        click.echo(
+            f"Dataset ready at: http://{apiURL}:{local_port}/#/viewer/{new_folder['_id']}"
+        )
 
 
 @upload.command()
 @upload_arguments
-@click.option(
-        "--parent-folder",
-        type=str,
-        help="The _id of girder folder that the new folder will be created in. Defaults to user root."
-    )
-def zip(parent_folder, folder_name, path, url, port):
+@upload_options
+def zip(parent_folder, folder_name, path, url, port, annotations_path: Path):
     """
     Create folder with FOLDER_NAME in PARENT_FOLDER and upload a zip file from PATH\n
 
     FOLDER_NAME is a unique name for the folder to be created \n
     PATH is the the local path for the zip file to be uploaded
     """
-    gc = login(url, port)
+    apiURL = url if url else "localhost"
+    local_port = port if port else 8010
+    gc = login(apiURL, local_port)
     new_folder = create_folder(parent_folder, folder_name, gc)
     if new_folder:
         gc.uploadFileToFolder(new_folder["_id"], path)
+        if annotations_path:
+            gc.uploadFileToFolder(new_folder["_id"], annotations_path)
+        click.echo(
+            f"Dataset ready at: http://{apiURL}:{local_port}/#/viewer/{new_folder['_id']}"
+        )
 # TODO unzip
