8934ab4e3d1e230ea5cb70b77fc403857b2df330
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2023-12-01 10:02:53 -0500

    S3 Import Processing Handling (#1387)
    * Adds better S3 import processing based on UMD/Janssen version of dive
    
    * add batch processing

--- server/dive_server/views_rpc.py
+++ server/dive_server/views_rpc.py
@@ -5,8 +5,11 @@ from girder.api.describe import Description, autoDescribeRoute
 from girder.api.rest import Resource
 from girder.constants import AccessType
 from girder.models.folder import Folder
+from girder.models.item import Item
 from girder.models.token import Token
 
+from dive_utils import asbool, fromMeta
+from dive_utils.constants import DatasetMarker, FPSMarker, MarkForPostProcess, TypeMarker
 from dive_utils.types import PipelineDescription
 
 from . import crud, crud_rpc
@@ -22,7 +25,9 @@ class RpcResource(Resource):
         self.route("POST", ("pipeline",), self.run_pipeline_task)
         self.route("POST", ("train",), self.run_training)
         self.route("POST", ("postprocess", ":id"), self.postprocess)
+        self.route("POST", ("convert_dive", ":id"), self.convert_dive)
         self.route("POST", ("convert_large_image", ":id"), self.convert_large_image)
+        self.route("POST", ("batch_postprocess", ":id"), self.batch_postprocess)
 
     @access.user
     @autoDescribeRoute(
@@ -136,6 +141,64 @@ class RpcResource(Resource):
 
     @access.user
     @autoDescribeRoute(
+        Description("Post-processing to be run after media/annotation import")
+        .modelParam(
+            "id",
+            description="Item ID containing the file to process",
+            model=Item,
+            level=AccessType.WRITE,
+        )
+        .param(
+            "skipJobs",
+            "Whether to skip processing that might dispatch worker jobs",
+            paramType="formData",
+            dataType="boolean",
+            default=False,
+            required=False,
+        )
+        .param(
+            "skipTranscoding",
+            "Whether to skip processing that might dispatch worker jobs",
+            paramType="formData",
+            dataType="boolean",
+            default=True,
+            required=False,
+        )
+    )
+    def convert_dive(self, item, skipJobs, skipTranscoding):
+        # Get the parent folder and create a new subFolder then move the item into that folder.
+        parentFolder = Folder().findOne({"_id": item["folderId"]})
+        user = self.getCurrentUser()
+        if parentFolder:
+            foldername = f'Video {item["name"]}'
+            destFolder = Folder().createFolder(
+                parentFolder, foldername, creator=user, reuseExisting=True
+            )
+            Item().move(item, destFolder)
+            if not asbool(fromMeta(destFolder, DatasetMarker)):
+                destFolder["meta"].update(
+                    {
+                        TypeMarker: 'video',
+                        FPSMarker: -1,  # auto calculate the FPS from import
+                    }
+                )
+                Folder().save(destFolder)
+                crud_rpc.postprocess(self.getCurrentUser(), destFolder, skipJobs, skipTranscoding)
+            return str(destFolder['_id'])
+        return ''
+
+    def get_marked_for_postprocess(self, folder, user, datasets, limit):
+        subFolders = list(Folder().childFolders(folder, 'folder', user))
+        for child in subFolders:
+            if child.get('meta', {}).get(MarkForPostProcess, False):
+                if len(datasets) < limit:
+                    datasets.append(child)
+                else:
+                    return
+            self.get_marked_for_postprocess(child, user, datasets, limit)
+
+    @access.user
+    @autoDescribeRoute(
         Description("Convert folder of images to large images").modelParam(
             "id",
             description="Folder containing the items to process",
@@ -145,3 +208,47 @@ class RpcResource(Resource):
     )
     def convert_large_image(self, folder):
         return crud_rpc.convert_large_image(self.getCurrentUser(), folder)
+
+
+    @access.user
+    @autoDescribeRoute(
+        Description("Post-processing for after S3 Imports")
+        .modelParam(
+            "id",
+            description="Folder containing the items to process",
+            model=Folder,
+            level=AccessType.WRITE,
+        )
+        .param(
+            "skipJobs",
+            "Whether to skip processing that might dispatch worker jobs",
+            paramType="formData",
+            dataType="boolean",
+            default=False,
+            required=False,
+        )
+        .param(
+            "skipTranscoding",
+            "Whether to skip processing that might dispatch worker jobs",
+            paramType="formData",
+            dataType="boolean",
+            default=False,
+            required=False,
+        )
+        .param(
+            "limit",
+            "Number of Jobs to start to attempt to convert to DIVE format",
+            paramType="formData",
+            dataType="integer",
+            default=100,
+            required=False,
+        )
+    )
+    def batch_postprocess(self, folder, skipJobs, skipTranscoding, limit):
+        # get a list of possible Datasets
+        datasets = []
+        self.get_marked_for_postprocess(folder, self.getCurrentUser(), datasets, limit)
+        for subFolder in datasets:
+            subFolder['meta']['MarkForPostProcess'] = False
+            Folder().save(subFolder)
+            crud_rpc.postprocess(self.getCurrentUser(), subFolder, skipJobs, skipTranscoding)
