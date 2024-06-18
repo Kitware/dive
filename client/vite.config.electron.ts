3ef084ce2eaa890a0c46e53e95f832bb9f33de98
Bryon Lewis <Bryon.Lewis@kitware.com>
2024-06-18 08:39:08 -0400

    starting to utilize different vite configs for different builds

---
+++ client/vite.config.electron.ts
@@ -0,0 +1,68 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue2';
+import electron from 'vite-plugin-electron';
+import { gitDescribeSync } from 'git-describe';
+import path, { resolve } from 'path';
+
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import http from 'http';
+
+import packagejson from './package.json';
+
+const gitHash = gitDescribeSync().hash;
+const { version } = packagejson;
+const keepAliveAgent = new http.Agent({ keepAlive: true });
+const staticPath = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_STATIC_PATH || './static/dive' : './';
+
+export default defineConfig({
+  base: './',
+  optimizeDeps: { include: ['axios', 'qs', 'markdown-it', 'js-cookie'] },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver(),
+      ],
+      // Don't exclude girder web components
+      exclude: [
+        /[\\/]node_modules[\\/](?!(@girder[\\/]components[\\/]|\.pnpm[\\/]@girder.*))/,
+        /[\\/]\.git[\\/]/,
+        /[\\/]\.nuxt[\\/]/,
+      ],
+    }),
+    electron({
+      main: {
+        entry: 'platform/desktop/background.ts',
+        vite: {
+          build: {
+            sourcemap: true,
+            outDir: 'dist_electron/main',
+          },
+        },
+      },
+      renderer: {
+        entry: 'platform/desktop/main.ts', // Use a different entry point for Electron renderer
+        vite: {
+          build: {
+            sourcemap: true,
+            outDir: 'dist_electron/renderer',
+          },
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      'vue-media-annotator': path.resolve(__dirname, './src'),
+      'dive-common': path.resolve(__dirname, './dive-common'),
+      platform: path.resolve(__dirname, './platform'),
+    },
+  },
+  define: {
+    'process.env.VITE_APP_GIT_HASH': JSON.stringify(gitHash),
+    'process.env.VITE_APP_VERSION': JSON.stringify(version),
+    'process.env.VITE_BUILD_TARGET': 'electron',
+  },
+});
