73a7e8dbbdd5f75e1b460d6b59cb3e8a677c3945
Matt Dawkins <matt.dawkins@kitware.com>
2026-01-07 20:01:31 -0500

    Fix linting issue

--- client/dive-common/components/Viewer.vue
+++ client/dive-common/components/Viewer.vue
@@ -25,11 +25,12 @@ import { provideAnnotator } from 'vue-media-annotator/provides';
 import {
   ImageAnnotator,
   VideoAnnotator,
-  NativeVideoAnnotator,
   LargeImageAnnotator,
   LayerManager,
   useMediaController,
 } from 'vue-media-annotator/components';
+// NativeVideoAnnotator uses electron APIs, import directly to avoid lib build issues
+import NativeVideoAnnotator from 'vue-media-annotator/components/annotators/NativeVideoAnnotator.vue';
 import type { AnnotationId } from 'vue-media-annotator/BaseAnnotation';
 import { getResponseError } from 'vue-media-annotator/utils';
 
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -6,8 +6,8 @@
 
 import ImageAnnotator from './annotators/ImageAnnotator.vue';
 import VideoAnnotator from './annotators/VideoAnnotator.vue';
-import NativeVideoAnnotator from './annotators/NativeVideoAnnotator.vue';
 import LargeImageAnnotator from './annotators/LargeImageAnnotator.vue';
+// NativeVideoAnnotator is not exported - it depends on electron and is only for desktop app
 
 import Controls from './controls/Controls.vue';
 import EventChart from './controls/EventChart.vue';
@@ -33,7 +33,6 @@ export {
   /* Annotators */
   ImageAnnotator,
   VideoAnnotator,
-  NativeVideoAnnotator,
   LargeImageAnnotator,
   /* Controls */
   Controls,
