9edfb0fa8df0d5fa26666a96496ca5eaf27938f7
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-06-23 08:16:34 -0400

    fix electron builds with vuetify

--- client/platform/desktop/plugins/vuetify.js
+++ client/platform/desktop/plugins/vuetify.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify';
 import colors from 'vuetify/es5/util/colors';
 import { vuetifyConfig as girderVuetifyConfig } from '@girder/components/src';
 import { merge } from 'lodash';
