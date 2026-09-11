34187e14a36ee109ef6a2c3d2168dc477980a36d
Matt Dawkins <matt.dawkins@kitware.com>
2026-09-11 16:47:06 -0400

    Add desktop VIAME add-on manager using the shared catalog and installer

--- client/platform/desktop/frontend/components/ViewerLoader.vue
+++ client/platform/desktop/frontend/components/ViewerLoader.vue
@@ -2185,6 +2185,9 @@ export default defineComponent({
           <v-tab :to="{ name: 'settings' }">
             Settings<v-icon>mdi-cog</v-icon>
           </v-tab>
+          <v-tab :to="{ name: 'addons' }">
+            Add-Ons<v-icon>mdi-puzzle</v-icon>
+          </v-tab>
         </v-tabs>
       </template>
       <template #title-right>
