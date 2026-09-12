fa3ac934a3cb1628ba3d622f4fd5b5e5f7cc6e3b
Matt Dawkins <matt.dawkins@kitware.com>
2026-09-11 20:07:47 -0400

    Match Other tab styling and restore full-width toolbar layout

--- client/platform/desktop/frontend/components/NavigationBar.vue
+++ client/platform/desktop/frontend/components/NavigationBar.vue
@@ -13,7 +13,7 @@ export default defineComponent({
   props: { name: { type: String, default: 'DIVE' } },
   setup() {
     const router = useRouter();
-    const container = ref<HTMLElement>();
+    const container = ref<{ $el: HTMLElement }>();
     const width = ref(Infinity);
     const destinations = computed(() => {
       const items: DesktopDestination[] = desktopDestinations.filter((item) => router.getRoutes().some((route) => route.name === item.name));
@@ -33,9 +33,9 @@ export default defineComponent({
     let observer: ResizeObserver;
     onMounted(() => {
       if (!container.value) return;
-      width.value = container.value.clientWidth;
+      width.value = container.value.$el.clientWidth;
       observer = new ResizeObserver(([entry]) => { width.value = entry.contentRect.width; });
-      observer.observe(container.value);
+      observer.observe(container.value.$el);
     });
     onBeforeUnmount(() => observer?.disconnect());
     return { container, navigation, tabWidth };
@@ -45,17 +45,16 @@ export default defineComponent({
 
 <template>
   <v-app-bar app>
-    <div ref="container" class="desktop-navigation" :style="{ '--desktop-tab-width': tabWidth }">
-      <v-tabs icons-and-text show-arrows="never" class="desktop-nav-tabs" color="accent">
-        <template v-for="item in navigation.visible">
-          <job-tab v-if="item.name === 'jobs'" :key="item.name" />
-          <v-tab v-else :key="item.name" :to="{ name: item.name, params: item.params }" :title="item.title">
-            {{ item.label }}<v-icon>{{ item.icon }}</v-icon>
-          </v-tab>
-        </template>
-        <annotation-other-menu v-if="navigation.hidden.length" :destinations="navigation.hidden" />
-      </v-tabs>
-    </div>
+    <!-- Vuetify applies toolbar height and background styles to direct-child tabs. -->
+    <v-tabs ref="container" icons-and-text show-arrows="never" class="desktop-nav-tabs desktop-navigation" color="accent" :style="{ '--desktop-tab-width': tabWidth }">
+      <template v-for="item in navigation.visible">
+        <job-tab v-if="item.name === 'jobs'" :key="item.name" />
+        <v-tab v-else :key="item.name" :to="{ name: item.name, params: item.params }" :title="item.title">
+          {{ item.label }}<v-icon>{{ item.icon }}</v-icon>
+        </v-tab>
+      </template>
+      <annotation-other-menu v-if="navigation.hidden.length" :destinations="navigation.hidden" />
+    </v-tabs>
   </v-app-bar>
 </template>
 
