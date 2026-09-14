a68db562f832eb0f71b11533f08ff7cf78b7c252
BryonLewis <BryonLewis@users.noreply.github.com>
2026-09-14 17:42:06 +0000

    Deployed 7a8c8fa with MkDocs version: 1.5.3

---
+++ instructions/gifcreate.sh
@@ -0,0 +1,6 @@
+palette="/tmp/palette.png"
+
+filters="fps=10,scale=800:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
